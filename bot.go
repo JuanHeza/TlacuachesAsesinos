921d7490a8c6644ee4196def8fd935165812f941
JuanHeza <juanehza@hotmail.com>
2023-05-01 18:49:36 -0600

    bot funciones de entrada y salida

--- bot.go
+++ bot.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"strconv"
 	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
@@ -10,6 +11,7 @@ import (
 )
 
 type Step struct {
+	name     Inline
 	message  string
 	keyboard tgbotapi.InlineKeyboardMarkup
 }
@@ -19,111 +21,159 @@ type Registro struct {
 	Motivo         string
 	Company        string
 	Calle          string
-	NumeroExterior int16
+	NumeroExterior int
 	Folio          int64
 	AutoFabricante string
 	Color          string
 	FechaEntrada   time.Time
 	HoraEntrada    time.Time
+	FechaSalida    time.Time
+	HoraSalida     time.Time
 	Identificacion string
 	FotoVehiculo   string
 	Observaciones  string
 }
 
-func (rg *Registro) printEntradaMesage() string {
-	return fmt.Sprintf("Nombre:\n%s\n\nCompañia:\n%s\n\nMotivo:\n%s\n\nCalle:\n%s\n\nNumero Exterior:\n%v", rg.Nombre, rg.Company, rg.Motivo, rg.Calle, rg.NumeroExterior)
+func (rg *Registro) printEntradaMesage(msg string) string {
+	return fmt.Sprintf("%s\n\n%s:\n%s\n\n%s:\n%s\n\n%s:\n%s\n\n%s:\n%s\n\n%s:\n%v", msg, textos[idioma][const_rgtNom], rg.Nombre, textos[idioma][const_rgtCom], rg.Company, textos[idioma][const_rgtMot], rg.Motivo, textos[idioma][const_rgtCll], rg.Calle, textos[idioma][const_rgtExt], rg.NumeroExterior)
+}
+
+func (rg *Registro) printSalidaMesage(msg string) string {
+	y, m, d := rg.FechaSalida.Date()
+	h, min, s := rg.HoraSalida.Clock()
+	date, hour := "\n", "\n"
+	if !rg.FechaSalida.IsZero() {
+		date = fmt.Sprintf("%02d/%v/%04d", d, printMes(int(m)), y)
+	}
+	if !rg.HoraSalida.IsZero() {
+		hour = fmt.Sprintf("%02d:%02d:%02d", h, min, s)
+	}
+	return fmt.Sprintf("%s\n\n%s:\n%s\n\n%s:\n%s", msg, textos[idioma][const_sldFch], date, textos[idioma][const_sldHra], hour)
+}
+
+func (rg *Registro) fillRegistro(campo Inline, valor interface{}) {
+	switch campo {
+	case const_rgtNom:
+		rg.Nombre = fmt.Sprintf("%v", valor)
+	case const_rgtCom:
+		rg.Company = fmt.Sprintf("%v", valor)
+	case const_rgtCll:
+		rg.Calle = fmt.Sprintf("%v", valor)
+	case const_rgtExt:
+		num, err := strconv.Atoi(fmt.Sprintf("%v", valor))
+		if err == nil {
+			rg.NumeroExterior = num
+		}
+	case const_rgtMot:
+		rg.Motivo = fmt.Sprintf("%v", valor)
+	}
 }
 
 var (
-	homeMessage = Step{
-		message: textos["es"]["saludo"],
-		keyboard: tgbotapi.NewInlineKeyboardMarkup(
-			tgbotapi.NewInlineKeyboardRow(
-				tgbotapi.NewInlineKeyboardButtonData("Generar QR", string(const_qrCode)),
-			),
-			tgbotapi.NewInlineKeyboardRow(
-				tgbotapi.NewInlineKeyboardButtonData("Registrar Entrada", string(const_rgtDir)),
-			),
-			tgbotapi.NewInlineKeyboardRow(
-				tgbotapi.NewInlineKeyboardButtonData("Registrar Visitante", string(const_rgtVst)),
-			),
-			tgbotapi.NewInlineKeyboardRow(
-				tgbotapi.NewInlineKeyboardButtonData("Registrar Salida", string(const_rgtSld)),
-			),
-			tgbotapi.NewInlineKeyboardRow(
-				tgbotapi.NewInlineKeyboardButtonData("\xF0\x9F\x87\xAA\xF0\x9F\x87\xB8", string(const_langEs)),
-				tgbotapi.NewInlineKeyboardButtonData("\xF0\x9F\x87\xBA\xF0\x9F\x87\xB8", string(const_langEn)),
-			)),
+	actualQuery = ""
+	actualReg   = Registro{}
+	actualForm  Inline
+	messageId   int
+	chatId      int64
+	idioma      = const_langEs
+	homeMessage = func() Step {
+		return Step{
+			name:    const_home,
+			message: textos[idioma][const_home],
+			keyboard: tgbotapi.NewInlineKeyboardMarkup(
+				tgbotapi.NewInlineKeyboardRow(
+					tgbotapi.NewInlineKeyboardButtonData(textos[idioma][const_qrCode], string(const_qrCode)),
+				),
+				tgbotapi.NewInlineKeyboardRow(
+					tgbotapi.NewInlineKeyboardButtonData(textos[idioma][const_rgtDir], string(const_rgtDir)),
+				),
+				tgbotapi.NewInlineKeyboardRow(
+					tgbotapi.NewInlineKeyboardButtonData(textos[idioma][const_rgtVst], string(const_rgtVst)),
+				),
+				tgbotapi.NewInlineKeyboardRow(
+					tgbotapi.NewInlineKeyboardButtonData(textos[idioma][const_rgtSld], string(const_rgtSld)),
+				),
+				tgbotapi.NewInlineKeyboardRow(
+					tgbotapi.NewInlineKeyboardButtonData("\xF0\x9F\x87\xAA\xF0\x9F\x87\xB8", string(const_langEs)),
+					tgbotapi.NewInlineKeyboardButtonData("\xF0\x9F\x87\xBA\xF0\x9F\x87\xB8", string(const_langEn)),
+				)),
+		}
 	}
 
-	entradaMessage = Step{
-		message: "Ingrese la siguiente informacion",
-		keyboard: tgbotapi.NewInlineKeyboardMarkup(
-			tgbotapi.NewInlineKeyboardRow(
-				tgbotapi.NewInlineKeyboardButtonData("Nombre", string(const_qrCode)),
-			),
-			tgbotapi.NewInlineKeyboardRow(
-				tgbotapi.NewInlineKeyboardButtonData("Compañia", string(const_rgtDir)),
-			),
-			tgbotapi.NewInlineKeyboardRow(
-				tgbotapi.NewInlineKeyboardButtonData("Motivo", string(const_rgtDir)),
-			),
-			tgbotapi.NewInlineKeyboardRow(
-				tgbotapi.NewInlineKeyboardButtonData("Calle", string(const_rgtVst)),
-			),
-			tgbotapi.NewInlineKeyboardRow(
-				tgbotapi.NewInlineKeyboardButtonData("Numero Exterior", string(const_back)),
-			),
-			tgbotapi.NewInlineKeyboardRow(
-				tgbotapi.NewInlineKeyboardButtonData("Aceptar", string(const_back)),
-				tgbotapi.NewInlineKeyboardButtonData("Cancelar", string(const_back)),
-			)),
+	miniKeyboard = tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonData("Aceptar", string(const_ok)),
+		tgbotapi.NewInlineKeyboardButtonData("Cancelar", string(const_back)),
+	)
+
+	entradaMessage = func() Step {
+		return Step{
+			name:    const_entrada,
+			message: textos[idioma][const_entrada],
+			keyboard: tgbotapi.NewInlineKeyboardMarkup(
+				tgbotapi.NewInlineKeyboardRow(
+					tgbotapi.NewInlineKeyboardButtonData(textos[idioma][const_rgtNom], string(const_rgtNom)),
+				),
+				tgbotapi.NewInlineKeyboardRow(
+					tgbotapi.NewInlineKeyboardButtonData(textos[idioma][const_rgtCom], string(const_rgtCom)),
+				),
+				tgbotapi.NewInlineKeyboardRow(
+					tgbotapi.NewInlineKeyboardButtonData(textos[idioma][const_rgtMot], string(const_rgtMot)),
+				),
+				tgbotapi.NewInlineKeyboardRow(
+					tgbotapi.NewInlineKeyboardButtonData(textos[idioma][const_rgtCll], string(const_rgtCll)),
+				),
+				tgbotapi.NewInlineKeyboardRow(
+					tgbotapi.NewInlineKeyboardButtonData(textos[idioma][const_rgtExt], string(const_rgtExt)),
+				),
+				miniKeyboard),
+		}
 	}
 
-	visitanteMessage = Step{
-		message: "Ingrese la siguiente informacion",
-		keyboard: tgbotapi.NewInlineKeyboardMarkup(
-			tgbotapi.NewInlineKeyboardRow(
-				tgbotapi.NewInlineKeyboardButtonData("Marca del vehiculo", string(const_qrCode)),
-			),
-			tgbotapi.NewInlineKeyboardRow(
-				tgbotapi.NewInlineKeyboardButtonData("Color", string(const_rgtDir)),
-			),
-			tgbotapi.NewInlineKeyboardRow(
-				tgbotapi.NewInlineKeyboardButtonData("Fecha Entrada", string(const_rgtDir)),
-			),
-			tgbotapi.NewInlineKeyboardRow(
-				tgbotapi.NewInlineKeyboardButtonData("Hora Entrada", string(const_rgtVst)),
-			),
-			tgbotapi.NewInlineKeyboardRow(
-				tgbotapi.NewInlineKeyboardButtonData("Identificacion", string(const_back)),
-			),
-			tgbotapi.NewInlineKeyboardRow(
-				tgbotapi.NewInlineKeyboardButtonData("Vehiculo", string(const_back)),
-			),
-			tgbotapi.NewInlineKeyboardRow(
-				tgbotapi.NewInlineKeyboardButtonData("Observaciones", string(const_back)),
-			),
-			tgbotapi.NewInlineKeyboardRow(
-				tgbotapi.NewInlineKeyboardButtonData("Aceptar", string(const_back)),
-				tgbotapi.NewInlineKeyboardButtonData("Cancelar", string(const_back)),
-			)),
+	visitanteMessage = func() Step {
+		return Step{
+			name:    const_visitante,
+			message: textos[idioma][const_visitante],
+			keyboard: tgbotapi.NewInlineKeyboardMarkup(
+				tgbotapi.NewInlineKeyboardRow(
+					tgbotapi.NewInlineKeyboardButtonData("Marca del vehiculo", string(const_qrCode)),
+				),
+				tgbotapi.NewInlineKeyboardRow(
+					tgbotapi.NewInlineKeyboardButtonData("Color", string(const_rgtDir)),
+				),
+				tgbotapi.NewInlineKeyboardRow(
+					tgbotapi.NewInlineKeyboardButtonData("Fecha Entrada", string(const_rgtDir)),
+				),
+				tgbotapi.NewInlineKeyboardRow(
+					tgbotapi.NewInlineKeyboardButtonData("Hora Entrada", string(const_rgtVst)),
+				),
+				tgbotapi.NewInlineKeyboardRow(
+					tgbotapi.NewInlineKeyboardButtonData("Identificacion", string(const_back)),
+				),
+				tgbotapi.NewInlineKeyboardRow(
+					tgbotapi.NewInlineKeyboardButtonData("Vehiculo", string(const_back)),
+				),
+				tgbotapi.NewInlineKeyboardRow(
+					tgbotapi.NewInlineKeyboardButtonData("Observaciones", string(const_back)),
+				),
+				miniKeyboard),
+		}
 	}
 
-	salidaMessage = Step{
-		message: "Ingrese la siguiente informacion",
-		keyboard: tgbotapi.NewInlineKeyboardMarkup(
-			tgbotapi.NewInlineKeyboardRow(
-				tgbotapi.NewInlineKeyboardButtonData("Fecha Salida", string(const_qrCode)),
-			),
-			tgbotapi.NewInlineKeyboardRow(
-				tgbotapi.NewInlineKeyboardButtonData("Hora Salida", string(const_rgtDir)),
-			),
-			tgbotapi.NewInlineKeyboardRow(
-				tgbotapi.NewInlineKeyboardButtonData("Aceptar", string(const_back)),
-				tgbotapi.NewInlineKeyboardButtonData("Cancelar", string(const_back)),
-			)),
+	salidaMessage = func() Step {
+		return Step{
+			name:    const_salida,
+			message: textos[idioma][const_salida],
+			keyboard: tgbotapi.NewInlineKeyboardMarkup(
+				tgbotapi.NewInlineKeyboardRow(
+					tgbotapi.NewInlineKeyboardButtonData(textos[idioma][const_sldFch], string(const_sldFch)),
+				),
+				tgbotapi.NewInlineKeyboardRow(
+					tgbotapi.NewInlineKeyboardButtonData(textos[idioma][const_sldHra], string(const_sldHra)),
+				),
+				miniKeyboard),
+		}
 	}
+
 	msg tgbotapi.MessageConfig
 )
 
@@ -147,38 +197,48 @@ func botInit() *tgbotapi.BotAPI {
 		switch true {
 		case update.CallbackQuery != nil:
 			msg = tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, "")
-			msg = handleCallback(update, bot)
-			break
+			bot.Send(handleCallback(update, bot))
 		case update.Message != nil:
 			msg = tgbotapi.NewMessage(update.Message.Chat.ID, "")
-			msg = handleMessage(update)
-			break
+			msg, status := handleMessage(update)
+
+			sended, _ := bot.Send(msg)
+			if !status {
+				chatId = sended.Chat.ID
+				messageId = sended.MessageID
+				actualQuery = ""
+			}
 		default:
 			fmt.Println("UNKNOWN")
-			msg.Text = textos["es"]["error"]
+			msg.Text = textos[idioma][const_error]
 		}
-		bot.Send(msg)
 	}
 	return bot
 }
 
-func handleMessage(update tgbotapi.Update) tgbotapi.MessageConfig {
+func handleMessage(update tgbotapi.Update) (tgbotapi.Chattable, bool) {
 	log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 	fmt.Println(update.Message.From.LanguageCode)
 	switch update.Message.Text {
 	case "/start":
 		msg = startCommand(update.Message.Chat.ID)
-		break
 	default:
-		msg = tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("%v \xF0\x9F\x87\xBA\xF0\x9F\x87\xB8", update.Message.Text))
-		msg.ReplyToMessageID = update.Message.MessageID
+		if actualQuery != "" {
+			//msg = tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("%v \xF0\x9F\x87\xBA\xF0\x9F\x87\xB8", update.Message.Text))
+			//msg.ReplyToMessageID = update.Message.MessageID
+			//} else {
+			actualReg.fillRegistro(toInline(actualQuery), update.Message.Text)
+			data := entradaMessage()
+			return tgbotapi.NewEditMessageTextAndMarkup(chatId, int(messageId), actualReg.printEntradaMesage(data.message), data.keyboard), true
+		}
 	}
-	return msg
+	return msg, false
 }
 
 func startCommand(Id int64) tgbotapi.MessageConfig {
-	msg = tgbotapi.NewMessage(Id, homeMessage.message)
-	msg.ReplyMarkup = homeMessage.keyboard
+	home := homeMessage()
+	msg = tgbotapi.NewMessage(Id, home.message)
+	msg.ReplyMarkup = home.keyboard
 	return msg
 }
 
@@ -205,26 +265,73 @@ func handleCallback(update tgbotapi.Update, bot *tgbotapi.BotAPI) tgbotapi.Messa
 				go delaySecond(msgPic, bot) // very useful for interval polling
 			}
 		}
-		break
 	case const_rgtDir:
-		dir := Registro{}
-		msg := tgbotapi.NewEditMessageTextAndMarkup(update.CallbackQuery.From.ID, update.CallbackQuery.Message.MessageID, entradaMessage.message+"\n"+dir.printEntradaMesage(), entradaMessage.keyboard)
+		actualReg := Registro{}
+		actualForm = const_rgtDir
+		data := entradaMessage()
+		msg := tgbotapi.NewEditMessageTextAndMarkup(update.CallbackQuery.From.ID, update.CallbackQuery.Message.MessageID, actualReg.printEntradaMesage(data.message), data.keyboard)
 		bot.Send(msg)
-		break
+	case const_rgtVst:
+		data := visitanteMessage()
+		msg := tgbotapi.NewEditMessageTextAndMarkup(update.CallbackQuery.From.ID, update.CallbackQuery.Message.MessageID, actualReg.printEntradaMesage(data.message), data.keyboard)
+		bot.Send(msg)
+	case const_rgtSld:
+		data := salidaMessage()
+		msg := tgbotapi.NewEditMessageTextAndMarkup(update.CallbackQuery.From.ID, update.CallbackQuery.Message.MessageID, actualReg.printSalidaMesage(data.message), data.keyboard)
+		bot.Send(msg)
 	case const_back:
-		msg := tgbotapi.NewEditMessageTextAndMarkup(update.CallbackQuery.From.ID, update.CallbackQuery.Message.MessageID, homeMessage.message, homeMessage.keyboard)
+		home := homeMessage()
+		msg := tgbotapi.NewEditMessageTextAndMarkup(update.CallbackQuery.From.ID, update.CallbackQuery.Message.MessageID, home.message, home.keyboard)
 		bot.Send(msg)
-		break
+	case const_ok:
+		var data Step
+		switch actualForm {
+		case const_rgtDir:
+			data = entradaMessage()
+			data.message = actualReg.printEntradaMesage(data.message)
+		case const_rgtVst:
+			data = visitanteMessage()
+		case const_rgtSld:
+			data = salidaMessage()
+		default:
+			data = homeMessage()
+		}
+		msg := tgbotapi.NewEditMessageTextAndMarkup(update.CallbackQuery.From.ID, update.CallbackQuery.Message.MessageID, data.message, data.keyboard)
+		bot.Send(msg)
+	case const_langEs:
+		changeLenguage(update, *bot)
+	case const_langEn:
+		changeLenguage(update, *bot)
+	case const_sldHra:
+		actualReg.HoraSalida = time.Now()
+		data := salidaMessage()
+		msg := tgbotapi.NewEditMessageTextAndMarkup(update.CallbackQuery.From.ID, update.CallbackQuery.Message.MessageID, actualReg.printSalidaMesage(data.message), data.keyboard)
+		bot.Send(msg)
+	case const_sldFch:
+		actualReg.FechaSalida = time.Now()
+		data := salidaMessage()
+		msg := tgbotapi.NewEditMessageTextAndMarkup(update.CallbackQuery.From.ID, update.CallbackQuery.Message.MessageID, actualReg.printSalidaMesage(data.message), data.keyboard)
+		bot.Send(msg)
+	default:
+		actualQuery = update.CallbackData()
+		msg := tgbotapi.NewEditMessageTextAndMarkup(update.CallbackQuery.From.ID, update.CallbackQuery.Message.MessageID, inputMessage(toInline(actualQuery)), tgbotapi.NewInlineKeyboardMarkup(miniKeyboard))
+		bot.Send(msg)
 	}
 	return msg
-
 }
 
 func delaySecond(msgs []tgbotapi.Message, bot *tgbotapi.BotAPI) {
-	for _ = range time.Tick(time.Duration(const_delay) * time.Second) {
+	for range time.Tick(time.Duration(const_delay) * time.Second) {
 		for _, msx := range msgs {
 			delete := tgbotapi.NewDeleteMessage(msx.Chat.ID, msx.MessageID)
 			bot.Send(delete)
 		}
 	}
+}
+
+func changeLenguage(update tgbotapi.Update, bot tgbotapi.BotAPI) {
+	idioma = toInline(update.CallbackData())
+	home := homeMessage()
+	msg := tgbotapi.NewEditMessageTextAndMarkup(update.CallbackQuery.From.ID, update.CallbackQuery.Message.MessageID, home.message, home.keyboard)
+	bot.Send(msg)
 }
--- constants.go
+++ constants.go
@@ -11,30 +11,103 @@ import (
 type Inline string
 
 const (
+	const_langEn Inline = "en"
+	const_langEs Inline = "es"
+
 	const_qrCode Inline = "qrCode"
-	const_langEn Inline = "lenguageEn"
-	const_langEs Inline = "lenguageEs"
 	const_rgtSld Inline = "registerSalida"
 	const_rgtVst Inline = "registerVisitiante"
 	const_rgtDir Inline = "registerDirection"
-	const_back Inline = "back"
-	const_ Inline = ""
-	const_delay  int16  = 5
+
+	const_saludoEntrada Inline = "Se registrara la siguiente Entrada"
+	const_rgtNom        Inline = "nombre"
+	const_rgtCom        Inline = "company"
+	const_rgtMot        Inline = "motvio"
+	const_rgtCll        Inline = "calle"
+	const_rgtExt        Inline = "exterior"
+
+	const_rgt Inline = ""
+
+	const_back   Inline = "back"
+	const_ok     Inline = "ok"
+	const_saludo Inline = "saludo"
+
+	const_saludoSalida Inline = "Ingrese la siguiente informacion salida"
+	const_sldFch       Inline = "sldFch"
+	const_sldHra       Inline = "sldHra"
+
+	const_saludoVisitante Inline = "Ingrese la siguiente informacion visitante"
+	const_error           Inline = "error"
+	const_solicitud       Inline = "solicitud"
+	const_home            Inline = "home"
+	const_entrada         Inline = "entrada"
+	const_visitante       Inline = "visitante"
+	const_salida          Inline = "salida"
+	const_delay           int16  = 5
 )
 
 var (
 	telegram_token = os.Getenv("telegram_bot_token")
 	host_name      = os.Getenv("HOST_NAME")
 	secure_key     = os.Getenv("SECURE_STRING")
-	session_key     = os.Getenv("SESSION_STRING")
-	textos         = map[string](map[string]string){
-		"en": map[string](string){
-			"saludo": "hello",
-			"error":  "Unknow Error",
+	session_key    = os.Getenv("SESSION_STRING")
+	//textos[idioma][]
+	meses = map[Inline]([12]string){
+		const_langEs: [12]string{
+			"enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre",
 		},
-		"es": map[string](string){
-			"saludo": "hola",
-			"error":  "Error Desconocido",
+		const_langEn: [12]string{
+			"january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december",
+		},
+	}
+	textos = map[Inline](map[Inline]string){
+		const_langEn: map[Inline](string){
+			const_home:      "Hello and welcome to the service",
+			const_visitante: string(const_saludoVisitante),
+			const_salida:    string(const_saludoSalida),
+
+			const_qrCode: "Generate QR Code",
+			const_rgtSld: "Register Exit",
+			const_rgtVst: "Register Visit",
+			const_rgtDir: "Register Entrance",
+
+			const_saludo: "hello",
+			const_error:  "Unknow Error",
+
+			const_entrada:   string(const_saludoEntrada),
+			const_solicitud: "Please write the ",
+			const_rgtNom:    "Name",
+			const_rgtCom:    "Company Name",
+			const_rgtMot:    "Motive of Visit",
+			const_rgtCll:    "Street Name",
+			const_rgtExt:    "Exterior Number",
+
+			const_sldHra: "Register Exit hour",
+			const_sldFch: "Register Exit date",
+		},
+		const_langEs: map[Inline](string){
+			const_home:      "hola y bienvenido al servicio",
+			const_visitante: string(const_saludoVisitante),
+			const_salida:    string(const_saludoSalida),
+
+			const_qrCode: "Generar QR",
+			const_rgtSld: "Registrar Salida",
+			const_rgtVst: "Registrar Visitante",
+			const_rgtDir: "Registrar Entrada",
+
+			const_saludo: "hola",
+			const_error:  "Error Desconocido",
+
+			const_entrada:   string(const_saludoEntrada),
+			const_solicitud: "Por favor ingrese el ",
+			const_rgtNom:    "Nombre",
+			const_rgtCom:    "Compañia",
+			const_rgtMot:    "Motivo",
+			const_rgtCll:    "nombre de la Calle",
+			const_rgtExt:    "Numero exterior",
+
+			const_sldHra: "Registrar Hora de salida",
+			const_sldFch: "Registrar Fecha de salida",
 		},
 	}
 )
@@ -50,4 +123,12 @@ func print(data interface{}) {
 
 func toInline(data string) Inline {
 	return Inline(data)
+}
+
+func inputMessage(msg Inline) string {
+	return textos[idioma][const_saludo] + textos[idioma][msg]
+}
+
+func printMes(mes int) string {
+	return meses[idioma][mes-1]
 }
